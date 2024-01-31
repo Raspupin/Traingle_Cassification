44305ed97993af2d771a4e133533d2677385c5fe
Daniel Ermakov <danielermakov2012@gmail.com>
2024-02-01 01:54:00 +0200

    v1.3 Added Tests to all the types.

--- Traingle_Casification_class_1/src/test/java/Traingle_Casification_class_1/Traingle_Casification_class_1/AppTest.java
+++ Traingle_Casification_class_1/src/test/java/Traingle_Casification_class_1/Traingle_Casification_class_1/AppTest.java
@@ -15,11 +15,51 @@ public class AppTest {
 	@Test
   
 	public void equilateralTriangle(){
-        int a = 2;
-        int b = 2;
-        int c=2;
+        int a = 8;
+        int b = 8;
+        int c = 8;
         triangle.classifyTriangle(a,b,c);
         String expected =Triangle_Types.equilateral.toString();
+        String result = triangle.get_type().toString();
+        assertEquals(expected, result.toString());
+    }
+	@Test
+	public void isTriangle(){
+        int a = 2;
+        int b = 4;
+        int c = 3;
+        triangle.classifyTriangle(a,b,c);
+        String expected =Triangle_Types.isosceles.toString();
+        String result = triangle.get_type().toString();
+        assertEquals(expected, result.toString());
+    }
+	@Test
+	public void notTriangle(){
+        int a = 2;
+        int b = 2;
+        int c = 6;
+        triangle.classifyTriangle(a,b,c);
+        String expected =Triangle_Types.Nottriangle.toString();
+        String result = triangle.get_type().toString();
+        assertEquals(expected, result.toString());
+    }
+	@Test
+	public void errorForTriangle(){
+        int a = 0;
+        int b = -5;
+        int c = 5;
+        triangle.classifyTriangle(a,b,c);
+        String expected =Triangle_Types.ERROR.toString();
+        String result = triangle.get_type().toString();
+        assertEquals(expected, result.toString());
+    }
+	@Test
+	public void isoscelesTriangle(){
+        int a = 4;
+        int b = 4;
+        int c = 5;
+        triangle.classifyTriangle(a,b,c);
+        String expected =Triangle_Types.isosceles.toString();
         String result = triangle.get_type().toString();
         assertEquals(expected, result.toString());
     }
